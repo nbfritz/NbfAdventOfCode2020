bb20deabc60a969b1306c6cfa9eeb8be1cd6c93a
Nathan Fritz <nbfritz@gmail.com>
2020-12-05 23:18:42 +0000

    Final tweaks to runner.

---
+++ lib/runner.py
@@ -0,0 +1,38 @@
+import re
+import importlib
+import pytest
+
+
+def parse_input(day_input):
+  return re.sub(
+    "(\d+)-(\d+)", 
+    lambda p: ",".join(str(i) for i in range(int(p.group(1)), int(p.group(2)) + 1)), 
+    day_input.replace(" ", "")
+  ).split(",")
+
+
+def run_single_day(day_number):
+  try: 
+    day = importlib.import_module(f"days.day{day_number}")
+  except ModuleNotFoundError:
+    print("That day isn't implemented yet.")
+    exit()
+    
+  data = day.load_data()
+  print(f"Part 1: {day.part1(data)}")
+  print(f"Part 2: {day.part2(data)}")
+    
+
+def test_single_day(day_number):
+  pytest.main([f"tests/days/test_day{day_number}.py"])
+
+
+def run_days(day_numbers):
+  for day_number in day_numbers:
+    print("\n")
+    print("*"*80)
+    print(f"RUNNING DAY {day_number}")
+    print("*"*80)
+
+    test_single_day(day_number)
+    run_single_day(day_number)
--- main.py
+++ main.py
@@ -1,29 +1,4 @@
-import re
-import importlib
-
-import pytest
+from lib.runner import parse_input, run_days
 
 day_input = input("What day or days should I run (ie. 1,4-5,8)? ")
-day_numbers = re.sub(
-  "(\d+)-(\d+)", 
-  lambda p: ",".join(str(i) for i in range(int(p.group(1)), int(p.group(2)) + 1)), 
-  day_input.replace(" ", "")
-).split(",")
-
-for day_number in day_numbers:
-  try: 
-    day = importlib.import_module(f"days.day{day_number}")
-  except ModuleNotFoundError:
-    print("That day isn't implemented yet.")
-    exit()
-  
-  print("\n")
-  print("*"*80)
-  print(f"RUNNING DAY {day_number}")
-  print("*"*80)
-
-  pytest.main([f"tests/days/test_day{day_number}.py"])
-  
-  data = day.load_data()
-  print(f"Part 1: {day.part1(data)}")
-  print(f"Part 2: {day.part2(data)}")
+run_days(parse_input(day_input))
